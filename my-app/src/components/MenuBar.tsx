22b3166e2051ced36e2310569e7e1c8062a211e4
Derek Song <dereksong28@gmail.com>
2025-08-24 17:35:52 -0400

    Update MenuBar.tsx

--- my-app/src/components/MenuBar.tsx
+++ my-app/src/components/MenuBar.tsx
@@ -11,14 +11,14 @@ const MenuBar = () => {
 
 
   return (
-    <div className="sticky bottom-0 bg-gray-800 z-50">
-      <div className="flex gap-3 justify-between border-t border-gray-700 p-2">
-        <img src="/OurGreenwayCombinationMarkHorizontalWhite.svg" alt="Our Greenway Logo" className="max-h-9"/>
-        <button className="p-0 !bg-white !text-black dark:bg-gray-200 dark:text-black font-semibold rounded touch-manipulation select-none appearance-none border-none outline-none" onClick={() => navigate(targetPath)}>
-          {buttonLabel}
-        </button>
-      </div>
-    </div>
+<div className="sticky bottom-0 bg-gray-800 z-50">
+  <div className="flex gap-3 justify-between border-t border-gray-700 p-2">
+    <img src="/OurGreenwayCombinationMarkHorizontalWhite.svg" alt="Our Greenway Logo" className="max-h-9 ml-2 md:ml-0"/>
+    <button className="p-0 !bg-white !text-black dark:bg-gray-200 dark:text-black font-semibold rounded touch-manipulation select-none appearance-none border-none outline-none" onClick={() => navigate(targetPath)}>
+      {buttonLabel}
+    </button>
+  </div>
+</div>
   );
 };
 
